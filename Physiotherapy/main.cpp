f2746b0ca7f7f3e3c6ff85b0e0a77d80513cbb51
mohamedCUsamy <Mohamed.Mohamed054@eng-st.cu.edu.eg>
2025-04-06 00:06:05 +0200

    fixed same file reading issue and added test.txt

--- Physiotherapy/main.cpp
+++ Physiotherapy/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include "Scheduler.h"
 
 int main() {
-	string path= "C:\\Users\\user\\Desktop\\test.txt";
+	string path= "test.txt";
 	Scheduler s1(path);
 	s1.Print_info();
 }
